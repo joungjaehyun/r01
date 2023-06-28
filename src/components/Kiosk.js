47e9631b9c1ef247d398af857dc5158db3c6bfd9
gwanyongpark <joungjaehyun1008@gmail.com>
2023-06-28 15:11:57 +0900

    Update Kiosk.js

--- src/components/Kiosk.js
+++ src/components/Kiosk.js
@@ -3,10 +3,10 @@
 // 이를 방지하고자 변수를 외부에서 선언한다.
 
 const products = [
-    {pno:1, pname:"Americano", price:10000},
-    {pno:2, pname:"Latte", price:8000},
-    {pno:3, pname:"Milk Tea", price:4000},
-    {pno:4, pname:"Green Tea", price:2000},
+    { pno: 1, pname: "Americano", price: 10000 },
+    { pno: 2, pname: "Latte", price: 8000 },
+    { pno: 3, pname: "Milk Tea", price: 4000 },
+    { pno: 4, pname: "Green Tea", price: 2000 },
 ]
 
 const Kiosk = () => {
@@ -15,29 +15,29 @@ const Kiosk = () => {
         console.log(products)
     }
 
-    return ( 
+    return (
         <div className="w-full h-[100vh] bg-slate-600 flex">
 
-        <div className="w-2/3 bg-white">
-        <div className="text-4xl font-serif">Procucts</div>
-        <ul>
-            {products.map(p=>
-                <li key={p.pno} className="text-2xl underline m-3 p-3 bg-rose-300"
-                onClick={()=> {handleClickBuy(p)}}>
-                    {p.pno} - {p.pname} - {p.price}
-                    <button className="border-2 m-2 p-2 bg-orange-300">BUY</button>
-                </li>
+            <div className="w-2/3 bg-white">
+                <div className="text-4xl font-serif">Procucts</div>
+                <ul>
+                    {products.map(p =>
+                        <li key={p.pno} className="text-2xl underline m-3 p-3 bg-rose-300"
+                            onClick={() => { handleClickBuy(p) }}>
+                            {p.pno} - {p.pname} - {p.price}
+                            <button className="border-2 m-2 p-2 rounded-sm bg-orange-300">BUY</button>
+                        </li>
 
-            )}
-        </ul>
-        </div>
+                    )}
+                </ul>
+            </div>
 
-        <div className="w-1/3">
-        <div className="text-4xl font-serif">Cart</div>
-        </div>
+            <div className="w-1/3">
+                <div className="text-4xl font-serif">Cart</div>
+            </div>
 
         </div>
-     );
+    );
 }
- 
+
 export default Kiosk;
