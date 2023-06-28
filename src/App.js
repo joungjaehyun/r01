4da9e8599f33976c8fff11d548b755e3ff36fa8b
gwanyongpark <joungjaehyun1008@gmail.com>
2023-06-28 18:20:58 +0900

    step4

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Counter from "./components/Counter";
 import Counter2 from "./components/Counter2";
 import Kiosk from "./components/Kiosk";
+import Logo from "./components/Logo";
 
 function App() {
   return (
     <div>
+      <Logo></Logo>
       <Kiosk></Kiosk>
     </div>
   );
---
+++ src/components/Logo.js
@@ -0,0 +1,9 @@
+const Logo = () => {
+    return ( 
+        <div className="grid place-items-center">
+        <img  src={require('../image/Logo.png')}></img>
+        </div>
+     );
+}
+ 
+export default Logo;
