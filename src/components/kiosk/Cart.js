08947b1d1994a9599e3eafe323eec3020daf2d2f
gwanyongpark <joungjaehyun1008@gmail.com>
2023-06-30 13:17:36 +0900

    cart에 담는것까지

--- src/components/kiosk/Cart.js
+++ src/components/kiosk/Cart.js
@@ -1,15 +1,34 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const Cart = ({id,pname,price}) => {
 
     // 어제와 다른점 props가 들어온다.
-    const [Items, setItems] =useState([])
+    const [items, setItems] =useState([])
 
     console.log("Cart ",id, pname, price)
+
+    useEffect(()=>{
+        
+        console.log("Cart useEffect......")
+        // 아예없을때와 0일때는 안들어가게 해야되므로
+        if(!id ||id === 0){
+            return
+        }
+        setItems([...items,{id,pname,price, qty:1}])
+
+    },[id,pname,price])
 
     return ( 
     <div>
         <div className="text-5xl">Cart</div>
+        <div>
+            <ul>
+                {items.map( (cartItem,idx)=>
+                    <li key={idx}>{cartItem.id} - {cartItem.pname} - {cartItem.price} - {cartItem.qty}</li>
+
+                )}
+            </ul>
+        </div>
 
     </div> );
 }
--- src/components/kiosk/index.js
+++ src/components/kiosk/index.js
@@ -8,7 +8,8 @@ const Kiosk = () => {
     // 같은 글을 누를때 조회수 처리를 하려면 갱신이 되야되는데
     // 숫자면 아예 갱신이 안되버린다(useEffect가 일어나질않는다)
     // Component Base 객체지향과 비슷하다
-    const [target, setTarget] =useState({pno:0})
+    const [target, setTarget] = useState({pno:0})
+    const [wanted, setWanted] = useState(null)
 
     const requestViewProduct = (id) =>{
         console.log("requestViewProduct" + id)
@@ -18,6 +19,7 @@ const Kiosk = () => {
     const requestBuy=(product) =>{
         console.log("requestBuy...... ")
         console.log(product)
+        setWanted(product)
     }
 
 
@@ -25,7 +27,7 @@ const Kiosk = () => {
         <div>
             <ProductList requestViewProduct={requestViewProduct}></ProductList>
             <ProductDetail target={target} requestBuy={requestBuy}></ProductDetail>
-            <Cart></Cart>
+            <Cart {...wanted}></Cart>
         </div>
       );
 }
