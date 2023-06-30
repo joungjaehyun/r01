1ed8ac7d10571d3758f0ceb62582bec643aca124
gwanyongpark <joungjaehyun1008@gmail.com>
2023-06-30 14:47:26 +0900

    키오스크 조정

--- src/App.js
+++ src/App.js
@@ -3,11 +3,12 @@
 // 폴더명만 보고 코드를 짤수있다 => 혼란성을 줄일수있다.
 
 import Kiosk from "./components/kiosk";
+import Todo1 from "./components/todo1";
 // index를 namaing룰상 소문자로 작성  고전적인 방식(2~3년전)
 function App() {
   return (
     <div>
-      <Kiosk></Kiosk>
+      <Todo1></Todo1>
 
     </div>
   );
---
+++ src/components/todo1/index.js
@@ -0,0 +1,9 @@
+const Todo1 = () => {
+    return (
+        <>
+            <div className="text-5xl">Todo 1</div>
+        </>
+        );
+}
+
+export default Todo1;
